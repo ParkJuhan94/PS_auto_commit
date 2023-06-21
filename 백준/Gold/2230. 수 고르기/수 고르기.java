9774aa201fe0bdbd861ef97a1a2ff98407b392c1
ParkJuHan <1004sunye@naver.com>
2023-06-21 18:12:50 +0900

    [Gold V] Title: 수 고르기, Time: 368 ms, Memory: 27600 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,50 @@
+//package WEEK12.P2230;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int p1, p2;
+    static int min;
+
+    public static void main(String[] args) throws IOException {
+      //  System.setIn(new FileInputStream("src/WEEK12/P2230/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        min = Integer.MAX_VALUE;
+        p1 = 0;
+        p2 = 0;
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        while(p1 < N && p2 < N){
+            int sub = arr[p2] - arr[p1];
+            if(sub == M){
+                System.out.println(M);
+                return;
+            } else if(sub < M){
+                p2++;
+            } else {
+                if(sub < min){
+                    min = sub;
+                }
+                p1++;
+            }
+        }
+
+        System.out.println(min);
+    }
+}
