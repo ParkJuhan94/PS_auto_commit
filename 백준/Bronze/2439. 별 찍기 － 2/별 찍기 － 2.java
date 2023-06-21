ea25ca780e1800188f2577db6de7fed21ac2dd0c
ParkJuHan <1004sunye@naver.com>
2023-06-21 20:50:04 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 276 ms, Memory: 19820 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,37 @@
+//package WEEK0.P90;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+//System.setIn(new FileInputStream("src/WEEK0/P90/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= N; i++){
+            for(int j = N - i; j >= 1; j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+//        int ans = 0;
+//
+//
+//        System.out.println(ans);
+    }
+
+}
