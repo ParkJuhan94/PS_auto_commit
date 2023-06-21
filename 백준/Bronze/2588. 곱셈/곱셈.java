5768676bea8938816ca384c41f5d9232baf87de7
ParkJuHan <1004sunye@naver.com>
2023-06-21 20:50:53 +0900

    [Bronze III] Title: 곱셈, Time: 116 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,28 @@
+//package WEEK0.P90;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        //System.setIn(new FileInputStream("src/WEEK0/P/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(br.readLine());
+
+        System.out.println(N * (M % 10));
+        System.out.println(N * ((M / 10) % 10));
+        System.out.println(N * (M / 100));
+        System.out.println(N * M);
+    }
+
+}
