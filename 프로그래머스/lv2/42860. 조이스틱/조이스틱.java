98bcd318241fb8a7f49f4be64e0209109a144c6f
ParkJuHan <1004sunye@naver.com>
2023-06-27 17:32:21 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int[] diff={0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1};
+        for(char c:name.toCharArray())
+            answer+=diff[c-'A'];
+
+        int length=name.length();
+        int min=length-1;
+
+        for(int i=0;i<length;i++){
+            int next=i+1;
+            while(next<length && name.charAt(next)=='A'){
+                next++;
+            }                
+            min=Math.min(min,i+length-next+Math.min(i,length-next));
+        }
+
+        return answer+min;
+    }
+}
