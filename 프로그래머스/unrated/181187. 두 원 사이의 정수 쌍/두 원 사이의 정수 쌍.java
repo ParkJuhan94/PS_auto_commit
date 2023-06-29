f5fd12a615ddd6c0c5f3a24791ac044249d9b165
ParkJuHan <1004sunye@naver.com>
2023-06-29 17:52:16 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 18.98 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -3,25 +3,21 @@ class Solution {
         long answer = 0;
         long sum = 0;   // 1사분면 위의 정수 쌍 갯수
 
-        long firstPow = (long)r1 * r1;
-        long secondPow = (long)r2 * r2;
+        long r1x = (long)r1 * r1;
+        long r2x = (long)r2 * r2;
 
         for(int i = 1; i <= r2; i++){
             if(i < r1){
-                long tmp = sum;
-                double firstRange = Math.floor(Math.sqrt(firstPow - (long)i * i));
-                double secondRange = Math.floor(Math.sqrt(secondPow - (long)i * i));
+                double y1 = Math.floor(Math.sqrt(r1x - (long)i * i));
+                double y2 = Math.floor(Math.sqrt(r2x - (long)i * i));
 
-                if(isInteger(Math.sqrt(firstPow - (long)i * i))){
-                    sum += secondRange - firstRange + 1;
+                if(isInteger(Math.sqrt(r1x - (long)i * i))){
+                    sum += y2 - y1 + 1;
                 }else{
-                    sum += secondRange - firstRange;
+                    sum += y2 - y1;
                 }
-                //System.out.println(sum - tmp);
             }else{
-                long tmp = sum;
-                sum += Math.floor(Math.sqrt(secondPow - (long)i * i)) + 1;
-                //System.out.println(sum - tmp);
+                sum += Math.floor(Math.sqrt(r2x - (long)i * i)) + 1;
             }
         }
 
