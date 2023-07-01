d1022f137e4577de60a163d9bbffe0661774b170
ParkJuHan <1004sunye@naver.com>
2023-07-02 08:13:11 +0900

    [level 2] Title: 3 x n 타일링, Time: 32.19 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -0,0 +1,28 @@
+//package Programmers.Level_2._3xn타일링;
+
+// https://bbeomgeun.tistory.com/30
+
+class Solution {
+
+    long[] dp;
+    int MOD = 1_000_000_007;
+
+    public int solution(int n) {
+
+        if(n % 2 == 1) return 0;
+
+        dp = new long[n + 1];
+        dp[0] = 1;
+        dp[2] = 3;
+
+        for(int i = 4 ; i <= n ; i+=2){
+            dp[i] = (dp[i-2] * 3);
+            for(int j = 0 ; j <= i-4 ; j += 2){
+                dp[i] += (dp[j] * 2) % MOD;
+            }
+            dp[i] %= MOD;
+        }
+
+        return (int)(dp[n] % MOD);
+    }
+}
