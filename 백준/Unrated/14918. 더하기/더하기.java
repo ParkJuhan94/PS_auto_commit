d2a6009fddc60fff6c720623202f7954d74fb5dd
ParkJuHan <1004sunye@naver.com>
2023-07-04 00:18:50 +0900

    [Unrated] Title: 더하기, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.java
@@ -0,0 +1,32 @@
+//package BOJ.Section04.P1103;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] map;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+       // System.setIn(new FileInputStream("src/BOJ/Section04/P1103/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        System.out.print(N + M);
+
+
+
+
+
+        // 무한번이면 -1
+    }
+
+}
