0c93ab06e3e32a52c35713cd67f2592c5744e7f0
ParkJuHan <1004sunye@naver.com>
2023-07-28 12:50:43 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 192 ms, Memory: 14868 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -13,7 +13,7 @@ public class Main {
     static int[] dc = {1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
-        //System.setIn(new FileInputStream("src/BOJ/Section06/P14499/input.txt"));
+     //   System.setIn(new FileInputStream("src/BOJ/Section06/P14499/input.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -33,7 +33,7 @@ public class Main {
 
         // top, up, down, left, right, bottom
         Dice cur = new Dice(0, 1, 2, 3, 4, 5);
-        int[] dice = new int[6]; // 주사위 각 면의 값을 저장할 배열
+        int[] dice = new int[6]; // 주사위 각 면의 값을 저장
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < K; i++) {
@@ -61,6 +61,7 @@ public class Main {
                 next = new Dice(d, t, b, lt, rt, u);
             }
 
+            // 굴러갈 위치
             int nr = r + dr[k];
             int nc = c + dc[k];
 
@@ -97,3 +98,94 @@ class Dice {
         this.bottom = bottom;
     }
 }
+
+/*
+
+public class Main {
+
+	//  1
+	// 234
+	//  5
+	//  6
+	static int[] dice = new int[7];
+	static int n,m,x,y;
+	static int[][] map;
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		y = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		
+		int k = Integer.parseInt(st.nextToken());
+		
+		map = new int[n][m];
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<k; i++) {
+			int d = Integer.parseInt(st.nextToken());
+			move(d);
+		}
+		
+	}
+	
+	static void move(int d) {
+		int nx = x + dx[d-1];
+		int ny = y + dy[d-1];
+		if(nx <0 || ny < 0 || nx > m-1 || ny > n-1) return;
+		roll(d, nx, ny);
+		x = nx; y = ny;
+		
+	}
+
+	// 1 2 3 4 (동 서 남 북)
+	static void roll(int d, int x, int y) {
+		int tmp = dice[3];
+		switch(d) {
+		case 1:
+			dice[3] = dice[4];
+			dice[4] = dice[6];
+			dice[6] = dice[2];
+			dice[2] = tmp;
+			break;
+		case 2:
+			dice[3] = dice[2];
+			dice[2] = dice[6];
+			dice[6] = dice[4];
+			dice[4] = tmp;
+			break;
+		case 3:
+			dice[3] = dice[5];
+			dice[5] = dice[6];
+			dice[6] = dice[1];
+			dice[1] = tmp;
+			break;
+		case 4:
+			dice[3] = dice[1];
+			dice[1] = dice[6];
+			dice[6] = dice[5];
+			dice[5] = tmp;
+			break;
+		}
+		if(map[y][x] == 0) {
+			map[y][x] = dice[6];
+		} else {
+			dice[6] = map[y][x];
+			map[y][x] =0;
+		}
+		System.out.println(dice[3]);
+		
+	}
+}
+
+ */
