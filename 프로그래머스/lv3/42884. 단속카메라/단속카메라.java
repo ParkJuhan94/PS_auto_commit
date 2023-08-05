3da27fec8aaf18383d8befb35f3f60c8ebe5b7fa
ParkJuHan <1004sunye@naver.com>
2023-08-06 06:25:58 +0900

    [level 3] Title: 단속카메라, Time: 9.28 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+
+        // 1. 진입 지점으로 오름차순 정렬 2. 진출 지점으로 내림차순 정렬
+        // 진입 지점이 같은 경우를 생각해보자.
+        Arrays.sort(routes, (o1, o2) -> {
+            if(o1[0] != o2[0]){
+                return o1[0] - o2[0];
+            }else{
+                return o2[1] - o1[1];
+            }
+        });
+
+        int end = routes[0][1];    // 현재까지 체크한 루트의 진출 지점
+        answer = 1;
+        for(int i = 1; i < routes.length; i++){
+            int[] route = routes[i];
+            if(end >= route[1]){
+                end = route[1];
+            }
+            if(end < route[0]){
+                end = route[1];
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
