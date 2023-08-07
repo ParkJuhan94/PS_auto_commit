df99b4754f24803266f7b10237477013e237aebf
ParkJuHan <1004sunye@naver.com>
2023-08-07 11:18:00 +0900

    [level 3] Title: 기지국 설치, Time: 0.92 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
@@ -4,15 +4,17 @@ class Solution {
         int idx = 0;    // 기지국 인덱스
         int pos = 1;    // 탐색중인 인덱스
 
+        // 시간 복잡도를 위해서 N 탐색해야한다
         while(pos <= n){
             if(idx < stations.length && stations[idx] - w <= pos)
             {
+                // 전파 되는 경우 : pos 증가 + 다음 기지국으로 인덱스 이동
                 pos = stations[idx] + w + 1;
                 idx++;
-            }else
-            {
-                answer++;
+            }else {
+                // 전파 안되는 경우 : 기지국 설치 + pos 증가
                 pos += (2 * w + 1);
+                answer++;
             }
         }
 
