e5994515c47ce815ec3b67228b8231e78375687e
ParkJuHan <1004sunye@naver.com>
2023-08-21 12:52:33 +0900

    [level 3] Title: 불량 사용자, Time: 3.96 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
@@ -4,39 +4,50 @@ import java.util.*;
 
 class Solution {
     static HashSet<HashSet<String>> answer;
+    static String[] user_id;
+    static String[] banned_id;
+    static LinkedHashSet<String> tmpBanList = new LinkedHashSet<>();    // 추가한 순서를 보장
+
     public int solution(String[] user_id, String[] banned_id) {
         answer = new HashSet<>();
+        Solution.user_id = user_id;
+        Solution.banned_id = banned_id;
 
-        dfs(new LinkedHashSet<>(), user_id, banned_id);
+        dfs();
 
         return answer.size();
     }
 
 
-    private static void dfs(HashSet<String> hs, String[] user_id, String[] banned_id) {
-        if (hs.size() == banned_id.length) {
-            if (isBanList(hs, banned_id)) {
-                answer.add(new HashSet<>(hs));
+    private static void dfs() {
+        if (tmpBanList.size() == banned_id.length) {
+
+            if (isBanList()) {
+                answer.add(new HashSet<>(tmpBanList));
             }
             return;
         }
 
+        // 완탐
         for (String userId : user_id) {
-            if (hs.add(userId)) {
-                dfs(hs, user_id, banned_id);
-                hs.remove(userId);
+            if (tmpBanList.add(userId)) {
+                dfs();
+                tmpBanList.remove(userId);
             }
         }
     }
 
-
-    private static boolean isBanList(HashSet<String> hs, String[] banned_id) {
+    // 불량사용자 리스트에 맞는지 확인
+    private static boolean isBanList() {
         int idx = 0;
-        for (String userID : hs) {
+
+        for (String userID : tmpBanList) {
             String banID = banned_id[idx++];
+
             if (userID.length() != banID.length()) {
                 return false;
             }
+
             for (int i = 0; i < banID.length(); i++) {
                 if (banID.charAt(i) == '*') {
                     continue;
@@ -46,6 +57,7 @@ class Solution {
                 }
             }
         }
+
         return true;
     }
 }
