067717c6d04883175e253cf16e491746cfd40221
ParkJuHan <1004sunye@naver.com>
2023-10-09 21:34:14 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 56.43 ms, Memory: 218 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -0,0 +1,57 @@
+// package Programmers.Level_3.파괴되지않은건물;
+
+// N = 1000
+// M = 250,000
+
+class Solution {
+    public static int[][] sums;
+
+    public int solution(int[][] board, int[][] skill) {
+        int answer = 0;
+        int r = board.length;
+        int c = board[0].length;
+        sums = new int[r + 1][c + 1];
+
+        for(int[] s : skill){
+            int type = s[0];
+            if(type == 1){
+                sums[s[1]][s[2]] -= s[5];
+                sums[s[3] + 1][s[2]] += s[5];
+                sums[s[1]][s[4] + 1] += s[5];
+                sums[s[3] + 1][s[4] + 1] -= s[5];
+            } else {
+                sums[s[1]][s[2]] += s[5];
+                sums[s[3] + 1][s[2]] -= s[5];
+                sums[s[1]][s[4] + 1] -= s[5];
+                sums[s[3] + 1][s[4] + 1] += s[5];
+            }
+        }
+
+
+        // 왼 -> 오른
+        for(int i=0;i<r+1;i++){
+            for(int j=0;j<c;j++){
+                sums[i][j+1] += sums[i][j];
+            }
+        }
+
+
+
+        // 위 -> 아래
+        for(int j=0;j<c+1;j++){
+            for(int i=0;i<r;i++){
+                sums[i+1][j] += sums[i][j];
+            }
+        }
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(board[i][j] + sums[i][j] > 0) answer++;
+            }
+        }
+
+        return answer;
+    }
+
+
+}
