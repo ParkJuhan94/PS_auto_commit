7d32af920653556ecb3cd63f7f61a1d19f1d1841
ParkJuHan <1004sunye@naver.com>
2023-10-19 14:45:51 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,35 @@
+//package WEEK000.P9655;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[][] map;
+    static int[][] ch;
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+    static int ans = 0;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+       // System.setIn(new FileInputStream("src/WEEK000/P9655/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+
+        if(N % 2 == 0){
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+    }
+
+}
