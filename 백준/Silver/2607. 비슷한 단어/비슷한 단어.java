3e1874dbcc5d7621e26bd8e40a0ff94665666ca4
ParkJuHan <1004sunye@naver.com>
2023-11-06 21:01:11 +0900

    [Silver II] Title: 비슷한 단어, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -13,7 +13,7 @@ public class Main {
         String standard = br.readLine();
         int len = standard.length();
         int[] alphabet = new int[26];
-        
+
         for (int i = 0; i < len; i++) {
             alphabet[standard.charAt(i) - 'A']++;
         }
@@ -23,7 +23,7 @@ public class Main {
             int[] temp = alphabet.clone();
             String comp = br.readLine();
             int cnt = 0;
-            
+
             for (int i = 0; i < comp.length(); i++) {
                 if (temp[comp.charAt(i) - 'A'] > 0) {
                     cnt++;
@@ -31,11 +31,17 @@ public class Main {
                 }
             }
 
-            if (len - 1 == comp.length() && cnt == comp.length()) { //길이가 -1
+            if (len - 1 == comp.length() && cnt == comp.length())
+            {   //길이가 -1
+                //기준이 되는 문자열에서 한 글자를 뺀다
                 ans++;
-            } else if (len == comp.length()) { //길이가 equal
+            } else if (len == comp.length())
+            {   //길이가 equal
+                //한 글자를 다른 문자를 바꿀수 있으니까 -1만큼만 일치해도 가능
                 if (cnt == len || cnt == len - 1) ans++;
-            } else if (len + 1 == comp.length()) { //길이가 +1
+            } else if (len + 1 == comp.length())
+            {   //길이가 +1
+                //비교할 문자열에서 한 글자 뺀다
                 if (cnt == len) ans++;
             }
         }
