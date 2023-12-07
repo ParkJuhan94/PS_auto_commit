056d055942098909d410e65e43f998987f5bfe0e
ParkJuHan <1004sunye@naver.com>
2023-12-07 16:27:11 +0900

    Update 가르침.java

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,16 +1,18 @@
+//package BOJ.Section08.P1062;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
+
     static int N, K, i, selectedCount = 0, max = 0;
     static String[] words;
     static int res = 0;
-    static int ch[] = new int[26];
+    static int[] ch = new int[26];
 
     public static void main(String[] args) throws FileNotFoundException {
-        //System.setIn(new FileInputStream("src/DAY01/P1062/input.txt"));
+      //  System.setIn(new FileInputStream("src/BOJ/Section08/P1062/input.txt"));
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
@@ -18,74 +20,77 @@ public class Main {
 
         words = new String[N];
 
-        if(K<5) {
+        if (K < 5) {
             System.out.println(0);
             return;
-        }else if(K==26) {
+        } else if (K == 26) {
             System.out.println(N);
             return;
-        }else{
+        } else {
             ch['a' - 'a'] = 1;
             ch['n' - 'a'] = 1;
             ch['t' - 'a'] = 1;
             ch['c' - 'a'] = 1;
             ch['i' - 'a'] = 1;
-
-            //for (i = 0; i < N; i++) words[i] = sc.next();
+            
             //입력받을 때 a n t i c를 공백으로 치환해서 속도 향상
-            for(i = 0; i < N; i++){
+            for (i = 0; i < N; i++) {
                 words[i] = sc.next().replaceAll("[antic]", "");
             }
 
             selectedCount = 5;
+//            max = 0; // 틀림. K == 5일 때 dfs 안 돌리니까    
             max = countWords();
 
-            for(i = 0 ; i < 26; i++){
-                if(ch[i] == 0) dfs(i);
+            for (i = 0; i < 26; i++) {
+                if (ch[i] == 0) {
+                    dfs(i);
+                }
             }
         }
 
         System.out.println(max);
     }
 
-    //반드시 main 처럼 static 으로 만든다.
-    static void dfs(int idx){
+    static void dfs(int idx) {
         //1. 체크인하면서 count 증가
         ch[idx] = 1;
         selectedCount++;
 
         //2. 목적지인가?
-        if(selectedCount == K){
+        if (selectedCount == K) {
             max = Math.max(max, countWords());
-        }else{
+        } else {
             //3. 아니면 연결된 곳을 순회
-            for(int i = idx + 1; i <= 25; i++){
+            for (int i = idx + 1; i <= 25; i++) {
                 //4. 갈 수 있는가?
-                if(ch[i] == 0){
+                if (ch[i] == 0) {
                     //5. 간다
                     dfs(i);
                 }
-            }    
+            }
         }
-        
+
         //6. 체크아웃하면서 count 감소
         ch[idx] = 0;
         selectedCount--;
     }
 
-    static int countWords(){
+    static int countWords() {
         int cnt = 0;
-        for(int n = 0; n < N; n++){
+        for (int n = 0; n < N; n++) {
             boolean isPossible = true;
             String word = words[n];
-
-            for(int i = 0; i < word.length(); i++){
-                if(ch[word.charAt(i) - 'a'] == 0){
+            
+            for (int i = 0; i < word.length(); i++) {
+                if (ch[word.charAt(i) - 'a'] == 0) {
                     isPossible = false;
                     break;
                 }
             }
-            if(isPossible) cnt++;
+            if (isPossible) {
+                cnt++;
+            }
         }
         return cnt;
     }
