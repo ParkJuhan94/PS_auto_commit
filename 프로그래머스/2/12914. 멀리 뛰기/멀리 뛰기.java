2eff50b3a717f18a955aab6a58fa12a1d7ecdf60
박주한 <1004sunye@naver.com>
2024-09-12 21:17:05 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.24 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,17 @@
+class Solution {
+    static long[] dp;
+
+    public long solution(int n) {
+        dp = new long[2001];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+            dp[i] %= 1234567;
+        }
+
+        return dp[n];
+    }
+}
