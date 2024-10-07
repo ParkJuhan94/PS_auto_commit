db3816824d0985094f788222e0110c73e74dce63
박주한 <1004sunye@naver.com>
2024-10-08 06:39:32 +0900

    [level 0] Title: OX퀴즈, Time: 0.23 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -2,26 +2,18 @@ class Solution {
     public String[] solution(String[] quiz) {
         String[] answer = new String[quiz.length];
         
-        for(int i = 0; i < quiz.length; i++) {
+        for (int i = 0; i < quiz.length; i++) {
             String[] divideStr = quiz[i].split(" ");
             int x = Integer.parseInt(divideStr[0]);
             int y = Integer.parseInt(divideStr[2]);
             int z = Integer.parseInt(divideStr[4]);
-            String firstOperator = divideStr[1];
+            String operator = divideStr[1];
             
-            if(firstOperator.equals("+")) {
-                if(x + y == z) {
-                    answer[i] = "O";
-                } else {
-                    answer[i] = "X";
-                }
-            } else {
-                  if(x - y == z) {
-                    answer[i] = "O";
-                } else {
-                    answer[i] = "X";
-                }
-            }
+            // 연산 결과 계산
+            int result = operator.equals("+") ? x + y : x - y;
+            
+            // 결과 확인 후 "O" 또는 "X" 저장
+            answer[i] = result == z ? "O" : "X";
         }
         
         return answer;
